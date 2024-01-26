2a58a5af71015cb6127fb0c00f82928d7eef9ea3
Albert Filip <albert.filip30@yahoo.com>
2024-01-26 09:40:06 +0100

    Initial commit

---
+++ teller.js
@@ -0,0 +1,45 @@
+function setNum1(){
+    numToShow += 1;
+    outputDiv.innerHTML = numToShow;
+}
+
+function setNum2(){
+    numToShow -= 1;
+    outputDiv.innerHTML = numToShow;
+}
+
+
+
+function setNum10(){
+    numToShow += 10;
+    outputDiv.innerHTML = numToShow;
+}
+
+function setNum20(){
+    numToShow -= 10;
+    outputDiv.innerHTML = numToShow;
+}
+
+
+
+function setNum100(){
+    numToShow += 100;
+    outputDiv.innerHTML = numToShow;
+}
+
+function setNum200(){
+    numToShow -= 100;
+    outputDiv.innerHTML = numToShow;
+}
+
+
+
+function setNum1000(){
+    numToShow += 1000;
+    outputDiv.innerHTML = numToShow;
+}
+
+function setNum2000(){
+    numToShow -= 1000;
+    outputDiv.innerHTML = numToShow;
+}
